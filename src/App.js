f283ed0d606abcf057749d1b57090e43fe2a6e9e
taavirannamets <taavirannamets@gmail.com>
2022-11-19 16:42:45 +0200

    matching 2 compared cards

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Card from './components/Card'
 import './App.css'
 
 const cardImages = [
-	{ src: '/img/helmet-1.png' },
-	{ src: '/img/potion-1.png' },
-	{ src: '/img/ring-1.png' },
-	{ src: '/img/scroll-1.png' },
-	{ src: '/img/shield-1.png' },
-	{ src: '/img/sword-1.png' },
+	{ src: '/img/helmet-1.png', matched: false },
+	{ src: '/img/potion-1.png', matched: false },
+	{ src: '/img/ring-1.png', matched: false },
+	{ src: '/img/scroll-1.png', matched: false },
+	{ src: '/img/shield-1.png', matched: false },
+	{ src: '/img/sword-1.png', matched: false },
 ]
 
 function App() {
@@ -38,14 +38,23 @@ function App() {
 	useEffect(() => {
 		if (choiceOne && choiceTwo) {
 			if (choiceOne.src === choiceTwo.src) {
-				console.log('those cards match')
+				setCards(prevCards => {
+					return prevCards.map(card => {
+						if (card.src === choiceOne.src) {
+							return { ...card, matched: true }
+						} else {
+							return card
+						}
+					})
+				})
 				resetTurn()
 			} else {
-				console.log('those cards do not match')
 				resetTurn()
 			}
 		}
 	}, [choiceOne, choiceTwo])
+
+	console.log(cards)
 
 	// Reset choices & increase turn
 	const resetTurn = () => {
