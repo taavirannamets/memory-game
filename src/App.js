c90dd13611b1192fd4e71b0b69ebd8a9e852054f
taavirannamets <taavirannamets@gmail.com>
2022-11-19 17:23:51 +0200

    starting a new game automagically

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ function App() {
 			.sort(() => Math.random() - 0.5)
 			.map(card => ({ ...card, id: Math.random() }))
 
+		setChoiceOne(null)
+		setChoiceTwo(null)
 		setCards(shuffledCards)
 		setTurns(0)
 	}
@@ -64,6 +66,11 @@ function App() {
 		setTurns(prevTurns => prevTurns + 1)
 		setDisabled(false)
 	}
+
+	// Start a new game automagically
+	useEffect(() => {
+		shuffleCards()
+	}, [])
 
 	return (
 		<div className='App'>
