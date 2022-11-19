c5fdc24fd47086c77fde0bab098c4cc6f879e5ee
taavirannamets <taavirannamets@gmail.com>
2022-11-19 15:48:40 +0200

    created card grid

--- src/App.js
+++ src/App.js
@@ -30,6 +30,16 @@ function App() {
 		<div className='App'>
 			<h1>Magic Match</h1>
 			<button onClick={shuffleCards}>New game</button>
+			<div className='card-grid'>
+				{cards.map(card => (
+					<div className='card' key={card.id}>
+						<div>
+							<img className='front' src={card.src} alt='card fromt' />
+							<img className='back' src='/img/cover.png' alt='card back' />
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
